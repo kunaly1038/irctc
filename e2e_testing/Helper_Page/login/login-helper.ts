3deb9df2cad5c771e1e90b4a22ce8912f38db0a1
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2019-03-19 18:59:51 +0530

    Update login-helper.ts

--- e2e_testing/Helper_Page/login/login-helper.ts
+++ e2e_testing/Helper_Page/login/login-helper.ts
@@ -1,17 +1,35 @@
-import { LazyHelper } from "../lazy/lazy-helper-fun";
-import { LoginPageHelper } from "./login-helper-locate";
+import { PageHelper } from "../lazy/lazy-helper-fun";
+import { PageLocator } from "./login-helper-locate";
 import { LoginConstant } from "./login-constant"
 import { browser } from "protractor";
+
+
 export class LoginHelper{
 
-    static loginInto(){
-        LazyHelper.loginUser();
-        LazyHelper.click(LoginPageHelper.loginBTN);
-        browser.sleep(3000)
-        LazyHelper.sendKeys(LoginPageHelper.userameLogin, LoginConstant.LandingPage.UserName)
-        LazyHelper.sendKeys(LoginPageHelper.passwordLogin, LoginConstant.LandingPage.Passowrd)
-        browser.sleep(15000)
-        LazyHelper.click(LoginPageHelper.submitLogin);
-        browser.sleep(15000)
+    static async loginInto(){
+        await PageHelper.navigateURL();
+        PageLocator.optionalDrop.isDisplayed().then(async bool => {
+            console.log('=============================='+bool+'=======================================')
+            if(bool==true){
+                await PageHelper.click(PageLocator.optionalDrop)
+                await PageHelper.click(PageLocator.optionalloginBTN);
+                await browser.sleep(3000)
+                await PageHelper.sendKeys(PageLocator.userameLogin, LoginConstant.LandingPage.UserName)
+                await PageHelper.sendKeys(PageLocator.passwordLogin, LoginConstant.LandingPage.Passowrd)
+                await browser.sleep(10000)
+                await PageHelper.click(PageLocator.submitLogin);
+                await browser.sleep(3000)
+                
+            }
+            else{
+                await PageHelper.click(PageLocator.loginBTN);
+                await browser.sleep(3000)
+                await PageHelper.sendKeys(PageLocator.userameLogin, LoginConstant.LandingPage.UserName)
+                await PageHelper.sendKeys(PageLocator.passwordLogin, LoginConstant.LandingPage.Passowrd)
+                await browser.sleep(10000)
+                await PageHelper.click(PageLocator.submitLogin);
+                await browser.sleep(3000)
+            }
+        });
     }
 }
