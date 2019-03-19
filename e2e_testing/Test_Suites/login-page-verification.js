8d65fa7aa3521b25a193833dae5ee1ea1d806c87
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2019-03-19 18:58:51 +0530

    Update login-page-verification.js

--- e2e_testing/Test_Suites/login-page-verification.js
+++ e2e_testing/Test_Suites/login-page-verification.js
@@ -1,16 +1,22 @@
-import { LazyHelper } from "../Helper_Page/lazy/lazy-helper-fun";
-import { LoginPageHelper } from "../Helper_Page/login/login-helper-locate";
+import { PageHelper } from "../Helper_Page/lazy/lazy-helper-fun";
+import { PageLocator } from "../Helper_Page/login/login-helper-locate";
+import { LoginHelper } from "../Helper_Page/login/login-helper";
 import { browser } from "protractor";
-import { LoginHelper } from "../Helper_Page/login/login-helper"
+
 describe('angularjs homepage todo list', function () {
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         await LoginHelper.loginInto();
     });
 
-    it('should add a todo', function () {
-        browser.sleep(5000)
-        
+    it("Search for train", async () => {
+        await PageHelper.sendKeys(PageLocator.fromTravel, 'gzb');
+        browser.sleep(2000)
+        await PageHelper.click(PageLocator.selectOption)
+        await PageHelper.sendKeys(PageLocator.toTravel, 'lko');
+        browser.sleep(2000)
+        await PageHelper.click(PageLocator.selectOption)
+        browser.sleep(6000)
     });
   
 });
