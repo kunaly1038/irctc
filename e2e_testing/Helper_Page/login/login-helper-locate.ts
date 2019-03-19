4ef094401f9f7f6756adf128a5589b20e9a88ca3
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2019-03-19 19:00:07 +0530

    Update login-helper-locate.ts

--- e2e_testing/Helper_Page/login/login-helper-locate.ts
+++ e2e_testing/Helper_Page/login/login-helper-locate.ts
@@ -1,8 +1,13 @@
-import {element, By, by} from 'protractor';
+import {element, By} from 'protractor';
 
-export class LoginPageHelper{
-    static get loginBTN(){ return element(by.xpath("//*[text()=' LOGIN ']"))}
+export class PageLocator{
+    static get optionalDrop(){return element(By.css('[class="h_menu_drop_button hidden-xs"]'))}
+    static get loginBTN(){ return element(By.xpath("//*[text()=' LOGIN ']"))}
+    static get optionalloginBTN(){ return element(By.xpath("//*[text()='LOGIN']"))}
     static get userameLogin(){ return element(By.css('[id="userId"]'))}
     static get passwordLogin(){ return element(By.css('[id="pwd"]'))}
     static get submitLogin(){ return element(By.xpath('//*[text()="SIGN IN"]'))}    
+    static get fromTravel(){ return element(By.css('[placeholder="From*"]'))}
+    static get toTravel(){return element(By.css('[placeholder="To*"]'))}
+    static get selectOption(){return element(By.css('[class="ng-star-inserted ui-autocomplete-list-item ui-corner-all"]'))}
 }
